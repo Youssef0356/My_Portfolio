8a3f830eae2f565bc86891c2f4f474ae63ecf639
Youssef0356 <nejiyoussef081@gmail.com>
2024-02-01 15:42:04 +0100

    .

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  function toggleMute() {
+
+    var video=document.getElementById("myvideo");
+  
+    if(video.muted){
+      video.muted = false;
+    } else {
+      debugger;
+      video.muted = true;
+      video.play()
+    }
+  
+  }
+  
+  $(document).ready(function(){
+    setTimeout(toggleMute,3000);
+  })
