d11fe71133894e11d0ab174ebfc830a25e5a82e8
Youssef0356 <nejiyoussef081@gmail.com>
2024-02-01 22:44:26 +0100

    javascript

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+function load(url) {
+  // display loading image here...
+  document.getElementById('animation').visible = true;
+  // request your data...
+  var req = new XMLHttpRequest();
+  req.open("POST", url, true);
+
+  req.onreadystatechange = function () {
+      if (req.readyState == 4 && req.status == 200) {
+          // content is loaded...hide the gif and display the content...
+          if (req.responseText) {
+              document.getElementById('header').innerHTML = req.responseText;
+              document.getElementById('loadingImg').visible = false;
+          }
+      }
+  };
+  request.send(vars);
+}
+
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
