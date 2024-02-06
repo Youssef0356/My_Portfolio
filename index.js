d5eda4397d1781b9e41e2e8ca9ca015815031a87
Youssef0356 <nejiyoussef081@gmail.com>
2024-02-06 21:48:22 +0100

    .

--- index.js
+++ index.js
@@ -23,3 +23,21 @@ function myFunction() {
   $(document).ready(function(){
     setTimeout(toggleMute,3000);
   })
+  const sliderm = new Sliderm('#exampe-slider', {
+    arrow: true,
+    pagination: true,
+    grouping: false,
+    loop: true,
+    preview: false,
+    columns: 4,
+    duration: 1000,
+    spacing: 10,
+    align: 'center',
+  });
+  sliderm.on('slide.start', () => {
+    console.log('Just starting to slide!');
+  });
+  
+  sliderm.on('slide.end', () => {
+    console.log('The slider is stopped.');
+  });
